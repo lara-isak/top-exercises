0237608faceed877de902ba30bfb54b649b3f694
Lara Isak <luckasti.larry@gmail.com>
2021-06-01 15:04:41 +0200

    :alembic: theme changer exercise

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-let shoppingDone = prompt("Is shopping done for the week?", "");
-let childsAllowance;
+const html = document.querySelector('html');
+const iconButton = document.querySelector('button');
+const icon = document.querySelector('i');
+const title = document.querySelector('h1');
 
-console.log(typeof(shoppingDone));
-// An empty string converts to 0. A non-numeric string converts to NaN which is always false.
+function update(bgColor, textColor) {
+  html.style.backgroundColor = bgColor; 
+  html.style.color = textColor;
+} 
 
-if(shoppingDone === "yes") {
-  childsAllowance = 10;
-  alert(childsAllowance);
-}
-else {
-  childsAllowance = 5;
-  alert(childsAllowance);
-}
+iconButton.addEventListener('click', changeTheme);
+
+function changeTheme() {
+    if(icon.classList.contains('ri-moon-line')) {
+      update('black', 'white');
+      icon.classList.remove('ri-moon-line');
+      icon.classList.add('ri-sun-line');
+      iconButton.classList.add('darkButton');
+    }
+    else {
+      update('white', 'black');
+      icon.classList.remove('ri-sun-line');
+      icon.classList.add('ri-moon-line');
+      iconButton.classList.remove('darkButton');
+    }
+  }
+
+
+
